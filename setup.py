36f852b3c7deecc1031925bab856916cf432f527
Iván Rendo Barreiro <56588129+i-rb@users.noreply.github.com>
2020-06-10 14:30:22 +0200

    mejorado setup

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
-from setuptools import setup, find_packages
-
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
-
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+from setuptools import setup
 
 setup(
-    name="img4chaos",
-    version="0.0.1",
-    author="Ivan Rendo Barreiro",
-    author_email="irendo@yahoo.es",
-    description="A package to detect chaos",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/i-rb/img4chaos/",
-    packages=find_packages(),
-    install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    ],
+    name='img4chaos',
+    url='https://github.com/i-rb/img4chaos',
+    author='Ivan Rendo Barreiro',
+    author_email='irendo@yahoo.es',
+    packages=['img4chaos'],
+    install_requires=['numpy','pandas'], #more
+    version='0.1',
+    license='MIT',
+    description='A package for detecting chaos',
+    long_description=open('README.txt').read(),
 )
